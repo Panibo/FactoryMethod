987e8bce6bead67d069fa366f35012f067f9c7f2
Panibo <miro.mariapori@metropolia.fi>
2023-01-16 12:37:31 +0200

    Tehtävä

--- Factory Method/src/factorymethod/Main.java
+++ Factory Method/src/factorymethod/Main.java
@@ -4,6 +4,10 @@ public class Main {
 
     public static void main(String[] args) {
         AterioivaOtus opettaja = new Opettaja();
+        AterioivaOtus rehtori = new Rehtori();
+        AterioivaOtus opiskelija = new Opiskelija();
         opettaja.aterioi();
+        rehtori.aterioi();
+        opiskelija.aterioi();
     }
 }
